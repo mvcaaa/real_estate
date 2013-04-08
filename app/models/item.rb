212d3b367d9ff61a2797014fede1215e800b7860
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-08 21:47:52 +0700

    Model callbacks sample code

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,10 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
 
+  #after_initialize { puts 'Initialized' } # Item.new
+  #after_save {puts 'Saved'}               # Item.save create
+  #after_create {puts 'Created'}           # Item.create
+  #after_update {puts 'Updated'}
+  #after_destroy {puts 'Destroyed'}        # Item.destroy
+
 end
