0055535d23cf5d384d15199388d828f06f82fdfd
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-08 22:30:10 +0700

    Controller params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,9 @@ class ItemsController < ApplicationController
     render text: @items.map {|i| "#{i.name}, #{i.price}"}.join('<BR/>')
   end
 
+  def create
+   @item = Item.create(params[:item])
+   render text: "Item #{@item.name}, #{@item.price} created"
+  end
+
 end
