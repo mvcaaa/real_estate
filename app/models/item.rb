05ad8cb9f2f792af4ed85f1862e12a26e5380e07
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-11 01:06:01 +0700

    GeoCode implementation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :price, :description
+  attr_accessible :name, :price, :description, :address
+  geocoded_by :address, :units => :km
+  reverse_geocoded_by :latitude, :longitude
 
   validates :price, numericality: { greater_than: 0, allow_nil: true }
-  validates :name, :description, presence: true
+  validates :address, :name, :description, presence: true
 
-  #after_initialize { puts 'Initialized' } # Item.new
-  #after_save {puts 'Saved'}               # Item.save create
-  #after_create {puts 'Created'}           # Item.create
-  #after_update {puts 'Updated'}
-  #after_destroy {puts 'Destroyed'}        # Item.destroy
+  after_validation :geocode, :if => :address_changed?
+  # Идея хранить количество соседних домов в отдельном поле
+  #after_validation :nearbys_update, :if => :address_changed?
 
 end
