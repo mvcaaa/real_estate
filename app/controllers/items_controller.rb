64853f6d11fc9ffbca363afe0408008079f58419
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-10 18:45:34 +0700

    Templates added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,17 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render text: @items.map {|i| "#{i.name}, #{i.price}"}.join('<BR/>')
+  end
+
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render text: 'Page not found', status: 404
+    end
   end
 
   def create
-   @item = Item.create(params[:item])
-   render text: "Item #{@item.name}, #{@item.price} created"
+    @item = Item.create(params[:item])
+    render text: "Item #{@item.name}, #{@item.price} created"
   end
 
 end
